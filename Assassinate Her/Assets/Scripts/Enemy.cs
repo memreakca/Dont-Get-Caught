5f3c18649591ec6ab0631f936db664a68a2263cf
memreakca <memreakca7@gmail.com>
2023-08-22 17:31:57 +0300

    Tileset + enemyanims
    tileset enemy die idle walk anims and scripts

--- Assassinate Her/Assets/Scripts/Enemy.cs
+++ Assassinate Her/Assets/Scripts/Enemy.cs
@@ -10,19 +10,21 @@ public class Enemy : MonoBehaviour
     [Header("References")]
   
     [SerializeField] public Transform attackP;
-    //[SerializeField] public Animator animator;
+    [SerializeField] public Animator animator;
     [SerializeField] public LayerMask EnemyMask;
     
     [Header("Attributes")]
     [SerializeField] private float HP = 1f;
     [SerializeField] private float attackRange = 1f;
 
+    private bool isAlive = true;
     private void Update()
     {
         
         Collider2D[] hitplayer = Physics2D.OverlapCircleAll(attackP.position, attackRange, EnemyMask);
         foreach(Collider2D player in hitplayer)
         {
+            if(isAlive == true)
             AsassinMovement.main.die();
         }
     }
@@ -33,14 +35,21 @@ public class Enemy : MonoBehaviour
     public void takeDamage(float dmg)
     {
         HP -= dmg;
+        animator.SetTrigger("Hit");
         if (HP <= 0f)
         {
+            isAlive = false;
             Debug.Log("Enemy died");
+            Invoke("deadEnemy", 1.10f);
+
             //Destroy(gameObject); 
         }
        
     }
-
+    void deadEnemy()
+    {
+        Destroy(gameObject);
+    }
     private void OnCollisionEnter2D(Collision2D other)
     {
       
